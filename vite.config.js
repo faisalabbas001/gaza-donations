8fb7e36120a7aca927f455f4fd304752889ce7be
faisalabbas001 <faisalabbas7959@gmail.com>
2025-05-21 16:33:38 +0500

    new

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: path.join(process.cwd(), 'dist'),
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000
-  }
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
